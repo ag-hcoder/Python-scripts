8d43cd6acd5c10fbd4d5f75940bd01539687424b
ag-hcoder <73228724+ag-hcoder@users.noreply.github.com>
2020-10-27 12:29:57 +0530

    Add files via upload

--- automateEmail.py
+++ automateEmail.py
@@ -3,33 +3,38 @@ import pandas as pd
 import datetime
 import smtplib
 
-path = os.getwd()
+path = os.getcwd()
 os.chdir(path)
 
-email
-pwd
+# email
+# pwd
 
 
 def sendemail(to, sub, msg):
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login(email, pwd)
-    s.sendmail(email, to, f"Subject: {sub}\n\n{msg}")
-    s.quit()
+    print(f'This work {to} {sub} {msg}')
+    # s = smtplib.SMTP('smtp.gmail.com', 587)
+    # s.starttls()
+    # s.login(email, pwd)
+    # s.sendmail(email, to, f"Subject: {sub}\n\n{msg}")
+    # s.quit()
 
 
 if __name__ == '__main__':
     df = pd.read_excel('data.xlsx')
+    print(df)
     today = datetime.datetime.now().strftime("%d-%m")
+    print(today)
     currYear = datetime.datetime.now().strftime("%Y")
     writeInd = []
-    for ind, item in df:
-        if today == item['Birthday'] and currYear not in item['Year']:
+    for ind, item in df.iterrows():
+        bday = item['Birthday'].strftime("%d-%m")
+
+        if today == bday and currYear not in str(item['Year']):
             sendemail(item['email'], "Happy Birthday", item['msg'])
             writeInd.append(ind)
-
+    print(writeInd)
     for i in writeInd:
         yr = df.loc[i, 'Year']
-        df[i, 'Year'] = f"{yr}, {currYear}"
+        df.loc[i, 'Year'] = str(yr) + ',' + str(currYear)
 
-    df.to_excel('data.xlsx)
+    df.to_excel('data.xlsx')
